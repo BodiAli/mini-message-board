f3603e0f43ad119650a2c1b88e8a48dc62a4a890
Bodi Ali <bodiali59@gmail.com>
2024-10-28 04:26:06 +0300

    Setup routers for / && /new routes

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+const newMessageRouter = require("./routes/formRouter");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Routes
+app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
