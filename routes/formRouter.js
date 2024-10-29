03706ae4c471cf951a06fe4b0ccc424cd5b4b4ac
Bodi Ali <bodiali59@gmail.com>
2024-10-29 03:58:17 +0300

    Feat: Handle POST requests made to /new path to get form data and view it in the index page

--- routes/formRouter.js
+++ routes/formRouter.js
@@ -1,9 +1,18 @@
 const { Router } = require("express");
+const { messages } = require("../controllers/indexController");
 
 const newMessageRouter = Router();
 
 newMessageRouter.get("/", (req, res) => {
   res.render("pages/form");
+});
+
+newMessageRouter.post("/", (req, res) => {
+  const { messageText, messageUser } = req.body;
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+
+  res.redirect("/");
 });
 
 module.exports = newMessageRouter;
