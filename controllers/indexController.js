3e70862fa6e7194905850a891ba9343818f21622
Bodi Ali <bodiali59@gmail.com>
2024-10-31 05:28:01 +0300

    Add 2 more pseudo messages && add style variable to index.ejs

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -11,10 +11,22 @@ const messages = [
     text: "Hello World!",
     added: new Date(),
   },
+  {
+    id: crypto.randomUUID(),
+    user: "Jack",
+    text: "Hey!",
+    added: new Date(),
+  },
+  {
+    id: crypto.randomUUID(),
+    user: "Rue",
+    text: "Hi everyone!",
+    added: new Date(),
+  },
 ];
 
 function getIndexPage(req, res) {
-  res.render("pages/index", { title: "Mini Message Board", messages });
+  res.render("pages/index", { title: "Mini Message Board", messages, style: "index.css" });
 }
 
 module.exports = { getIndexPage, messages };
