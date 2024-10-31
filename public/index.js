488cf49f28ddffa3a7b7013501c5fc16b1a268d7
Bodi Ali <bodiali59@gmail.com>
2024-10-31 05:29:08 +0300

    Create a script file for index.ejs

---
+++ public/index.js
@@ -0,0 +1,34 @@
+const sectionElements = document.querySelectorAll("section");
+
+sectionElements.forEach((section) => {
+  section.addEventListener("click", (e) => {
+    const endpoint = `/message/${e.currentTarget.dataset.id}`;
+    window.location.href = endpoint;
+  });
+
+  const deleteButton = section.querySelector(".delete");
+  deleteButton.addEventListener("click", async (e) => {
+    const endpoint = `/message/${section.dataset.id}`;
+
+    try {
+      const response = await fetch(endpoint, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw new Error("An error occurred while trying to delete message");
+      }
+
+      window.location.reload();
+      return;
+    } catch (error) {
+      const errorElement = document.createElement("p");
+      errorElement.classList.add("error-message");
+      errorElement.textContent = error.message;
+      if (!section.querySelector(".error-message")) section.appendChild(errorElement);
+      setTimeout(() => {
+        errorElement.remove();
+      }, 5000);
+    }
+  });
+});
