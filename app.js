8e5994049b797a8ee266dd1588cbee1e96d1fa07
Bodi Ali <bodiali59@gmail.com>
2024-11-03 05:16:03 +0200

    Change status to 407

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(get404Page);
 
 // Error Handler
 app.use((err, req, res, _next) => {
-  res.status(404).render("pages/message-404", {
+  res.status(407).render("pages/message-404", {
     title: "Message Not Found",
     style: "message-404.css",
   });
